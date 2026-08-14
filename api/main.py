a7d6c442ceb64852cf51dffec72b94e833517303
XXOlegXXr <oleghresko2006@gmail.com>
2026-08-14 19:57:44 +0000

    last version

--- api/main.py
+++ api/main.py
@@ -1,58 +1,30 @@
 from fastapi import FastAPI
-import socket
+import subprocess
 
 app = FastAPI()
-
-AMI_HOST = "127.0.0.1"
-AMI_PORT = 5038
-AMI_USER = "alertapi"
-AMI_SECRET = "pass1234"
 
 PHONE = "+380980450537"
 
 
-def ami_command(command):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(5)
+def make_call():
+    cmd = [
+        "sudo",
+        "asterisk",
+        "-rx",
+        f"channel originate PJSIP/zadarma extension {PHONE}@alert-outgoing"
+    ]
 
-    s.connect((AMI_HOST, AMI_PORT))
-
-    login = (
-        "Action: Login\r\n"
-        f"Username: {AMI_USER}\r\n"
-        f"Secret: {AMI_SECRET}\r\n"
-        "Events: off\r\n"
-        "\r\n"
+    result = subprocess.run(
+        cmd,
+        capture_output=True,
+        text=True
     )
 
-    s.sendall(login.encode())
+    print("ASTERISK:")
+    print(result.stdout)
+    print(result.stderr)
 
-    response = s.recv(4096).decode()
-    print("AMI LOGIN:")
-    print(response)
-
-    originate = (
-        "Action: Originate\r\n"
-        "Channel: PJSIP/zadarma\r\n"
-        "Context: alert-outgoing\r\n"
-        f"Exten: {PHONE}\r\n"
-        "Priority: 1\r\n"
-        "CallerID: ALERT\r\n"
-        "Timeout: 30000\r\n"
-        "Async: true\r\n"
-        "\r\n"
-    )
-
-    s.sendall(originate.encode())
-
-    response = s.recv(4096).decode()
-
-    print("AMI ORIGINATE:")
-    print(response)
-
-    s.close()
-
-    return response
+    return result.returncode == 0
 
 
 @app.get("/")
@@ -63,18 +35,16 @@ def root():
 @app.post("/alert")
 def alert(data: dict):
 
-    print("ALERT RECEIVED")
-    print(data)
+    for alert in data.get("alerts", []):
 
-    for alert_data in data.get("alerts", []):
+        status = alert.get("status")
+        severity = alert.get("labels", {}).get("severity")
 
-        status = alert_data.get("status")
-        severity = alert_data.get("labels", {}).get("severity")
-
-        print(f"status={status}")
-        print(f"severity={severity}")
+        print(
+            f"ALERT: status={status}, severity={severity}"
+        )
 
         if status == "firing" and severity == "critical":
-            ami_command("originate")
+            make_call()
 
     return {"status": "received"}
